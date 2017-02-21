d395b813b007c26f8b83b60984f9e51f7ad7d9ec
Ev Dolzhenko <evgeniy.dolzhenko@blacksquaremedia.com>
2017-02-21 16:56:25 +0300

    More Rails 5ishness

--- lib/bsm/sso/client/url_helpers.rb
+++ lib/bsm/sso/client/url_helpers.rb
@@ -1,9 +1,8 @@
 module Bsm::Sso::Client::UrlHelpers
 
   def service_url(path = request.fullpath)
-    part = Regexp.escape params.slice(:ticket).to_query
+    part = Regexp.escape params.slice(:ticket).to_h.to_query
     request.base_url + path.sub(/#{part}\&?/, '').chomp("&").chomp("?")
   end
 
 end
-
